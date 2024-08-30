c8bd2397acb01057e338dd258402c0194d87d373
GiovannaBia <gioo@live.com.ar>
2024-08-30 14:29:28 -0300

    Agregar controlador y vistas de categoria (index y crear)

---
+++ Rocosa/Rocosa/Controllers/CategoriaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocosa.Datos;
+using Rocosa.Models;
+
+namespace Rocosa.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public CategoriaController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Categoria> lista = _db.Categoria;
+
+            return View(lista);
+        }
+
+        //Get (llama a la vista "Crear")
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Categoria categoria)
+        {
+            _db.Add(categoria);
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
