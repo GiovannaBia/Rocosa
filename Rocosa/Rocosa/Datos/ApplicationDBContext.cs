468c5060cd356dafab6985bbca13b129819a101c
GiovannaBia <gioo@live.com.ar>
2024-08-30 11:49:42 -0300

    Crecion base de datos, modelo y tabla categoria

---
+++ Rocosa/Rocosa/Datos/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rocosa.Models;
+
+namespace Rocosa.Datos
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {              
+        }
+        public DbSet<Categoria> Categoria { get; set; }
+    }
+}
