976c77ca0766329a5bb924633c84d01a97b3e7c9
GiovannaBia <gioo@live.com.ar>
2024-08-31 14:29:21 -0300

    Producto modelo

---
+++ Rocosa/Rocosa/Models/Producto.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Build.ObjectModelRemoting;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rocosa.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]  
+        public string NombreProducto { get; set; }
+
+        [Required(ErrorMessage = "La descripcion corta es requerida")]
+        public string DescripcionCorta { get; set; }
+
+        [Required(ErrorMessage = "La descripcion del producto es requerida")]
+        public string DescripcionProducto { get; set; }
+
+        [Required(ErrorMessage = "El precio del producto es requerido")]
+        [Range(1, double.MaxValue, ErrorMessage = "")]
+        public double Precio { get; set; }  
+
+        public string ImagenUrl { get; set; }
+
+        //Foreign Key
+        public int CategoriaId { get; set; }
+        [ForeignKey("CategoriaId")]
+        public virtual Categoria categoria { get; set; }   
+        public int TipoAplicacionId {  get; set; }
+        [ForeignKey("TipoAplicacionId")]
+        public virtual TipoAplicacion tipoAplicacion { get; set; }
+    }
+}
