fcec1e4a296a48df1262fa5dc4a2bb55426d64d1
GiovannaBia <gioo@live.com.ar>
2024-11-01 15:03:31 -0300

    Configuraccion e implementacion del manejo de Ventas

---
+++ Rocosa/Rocosa/Controllers/VentaController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Rocosa_AccesoDatos.Datos.Repositorio.IRepositorio;
+using Rocosa_Modelos.ViewModels;
+using Rocosa_Utilidades;
+
+namespace Rocosa.Controllers
+{
+    public class VentaController : Controller
+    {
+        private readonly IVentaRepositorio _ventaRepo;
+        private readonly IVentaDetalleRepositorio _ventaDetalleRepo;
+
+        public VentaController(IVentaRepositorio ventaRepo, IVentaDetalleRepositorio ventaDetalleRepo )
+        {
+                _ventaRepo = ventaRepo;
+                _ventaDetalleRepo = ventaDetalleRepo;
+        }
+
+        public IActionResult Index(string buscarNombre=null, string buscarEmail=null, string buscarTelefono = null, string Estado=null)
+        {
+            VentaVM ventaVM = new VentaVM()
+            {
+                VentaLista = _ventaRepo.ObtenerTodos(),
+                EstadoLista = WC.ListaEstados.ToList().Select(l => new SelectListItem
+                {
+                    Text = l,
+                    Value = l
+                })
+            };
+
+            if(!string.IsNullOrEmpty(buscarNombre))
+            {
+                ventaVM.VentaLista = ventaVM.VentaLista.Where(l=>l.NombreCompleto.ToLower().Contains(buscarNombre.ToLower())); 
+            }
+            if (!string.IsNullOrEmpty(buscarEmail))
+            {
+                ventaVM.VentaLista = ventaVM.VentaLista.Where(l => l.Email.ToLower().Contains(buscarEmail.ToLower()));
+            }
+            if (!string.IsNullOrEmpty(buscarTelefono))
+            {
+                ventaVM.VentaLista = ventaVM.VentaLista.Where(l => l.Telefono.ToLower().Contains(buscarTelefono.ToLower()));
+            }
+            if (!string.IsNullOrEmpty(Estado) && Estado != "--Estado--")
+            {
+                ventaVM.VentaLista = ventaVM.VentaLista.Where(l => l.EstadoVenta.ToLower().Contains(Estado.ToLower()));
+            }
+            return View(ventaVM);
+        }
+    }
+}
--- Rocosa/Rocosa_Utilidades/WC.cs
+++ Rocosa/Rocosa_Utilidades/WC.cs
@@ -1,4 +1,6 @@
-﻿namespace Rocosa_Utilidades
+﻿using System.Collections.ObjectModel;
+
+namespace Rocosa_Utilidades
 {
     public static class WC
     {
@@ -21,5 +23,11 @@
         public const string EstadoEnviado = "Enviado";
         public const string EstadoCancelado = "Cancelado";
         public const string EstadoDevuelto = "Devuelto";
+
+        public static readonly IEnumerable<string> ListaEstados = new ReadOnlyCollection<string>(
+            new List<string>
+            {
+                EstadoPendiente, EstadoAprobado, EstadoEnProceso, EstadoCancelado, EstadoDevuelto, EstadoEnviado
+            });
     }
 }
